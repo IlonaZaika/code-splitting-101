dbdd66a815b5390241f96320ea12ba63fd7f8c1b
IlonaZaika <zaika.ilona.v@gmail.com>
2024-11-29 11:32:57 +0000

    Implemented code splitting manually using the dynamic import() syntax.

--- code-splitting-101/src/App.js
+++ code-splitting-101/src/App.js
@@ -3,24 +3,40 @@ import "./App.css";
 import { useState } from "react";
 
 import Page1 from "./components/Page1";
-import Page2 from "./components/Page2";
-import Page3 from "./components/Page3";
 
 function App() {
   const [route, setRoute] = useState("page1");
+  const [Component, setComponent] = useState(null);
 
   const onRouteChange = (route) => {
-    setRoute(route);
+    if (route === "page1") {
+      setRoute(route);
+      setComponent(null);
+    } else if (route === "page2") {
+      import("./components/Page2")
+        .then((module) => {
+          setRoute(route);
+          setComponent(() => module.default);
+        })
+        .catch((err) => {});
+    } else {
+      import("./components/Page3")
+        .then((module) => {
+          setRoute(route);
+          setComponent(() => module.default);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   return (
     <>
       {route === "page1" ? (
         <Page1 onRouteChange={onRouteChange} />
-      ) : route === "page2" ? (
-        <Page2 onRouteChange={onRouteChange} />
       ) : (
-        <Page3 onRouteChange={onRouteChange} />
+        Component && <Component onRouteChange={onRouteChange} />
       )}
     </>
   );
