c760285e3734a7c7847b7e88e14adfe95430328f
IlonaZaika <zaika.ilona.v@gmail.com>
2024-12-02 14:37:28 +0000

    Added code splitting using React.lazy

--- code-splitting-101/src/App.js
+++ code-splitting-101/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, lazy, Suspense, Loading } from "react";
+import { useState, lazy, Suspense } from "react";
 
 import Page1 from "./components/Page1";
 const Page2 = lazy(() => import("./components/Page2"));
@@ -7,28 +7,14 @@ const Page3 = lazy(() => import("./components/Page3"));
 
 function App() {
   const [route, setRoute] = useState("page1");
-  const [Component, setComponent] = useState(null);
 
   const onRouteChange = (route) => {
     if (route === "page1") {
       setRoute(route);
-      setComponent(null);
     } else if (route === "page2") {
-      import("./components/Page2")
-        .then((module) => {
-          setRoute(route);
-          setComponent(() => module.default);
-        })
-        .catch((err) => {});
+      setRoute(route);
     } else {
-      import("./components/Page3")
-        .then((module) => {
-          setRoute(route);
-          setComponent(() => module.default);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      setRoute(route);
     }
   };
 
@@ -36,13 +22,21 @@ function App() {
     <>
       {route === "page1" ? (
         <Page1 onRouteChange={onRouteChange} />
+      ) : route === "page2" ? (
+        <Suspense fallback={<Loading />}>
+          <Page2 onRouteChange={onRouteChange} />
+        </Suspense>
       ) : (
         <Suspense fallback={<Loading />}>
-          <Component onRouteChange={onRouteChange} />
+          <Page3 onRouteChange={onRouteChange} />
         </Suspense>
       )}
     </>
   );
+}
+
+function Loading() {
+  return <h2>Loading...</h2>;
 }
 
 export default App;
