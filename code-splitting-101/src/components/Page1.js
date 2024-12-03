ef5e43cd9c118a9d279ccea0e4378f5298049039
IlonaZaika <zaika.ilona.v@gmail.com>
2024-12-03 14:45:01 +0000

    Implemented code splitting via router.

--- code-splitting-101/src/components/Page1.js
+++ code-splitting-101/src/components/Page1.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "./../logo.svg";
 
-const Page1 = ({ onRouteChange }) => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Welcome to React App</p>
-    </header>
-    <button className="disabled">Page1</button>
-    <button onClick={() => onRouteChange("page2")}>Page2</button>
-    <button onClick={() => onRouteChange("page3")}>Page3</button>
-  </div>
-);
+const Page1 = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to React App</p>
+      </header>
+      <button className="disabled">Page1</button>
+      <button onClick={() => navigate("/page2")}>Page2</button>
+      <button onClick={() => navigate("/page3")}>Page3</button>
+    </div>
+  );
+};
 
 export default Page1;
